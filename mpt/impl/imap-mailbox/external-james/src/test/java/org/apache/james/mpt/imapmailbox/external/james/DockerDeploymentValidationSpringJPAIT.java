d1e1aacf14a91ce7575f0608767b902993e4c5bd
Rémi KOWALSKI <rkowalski@linagora.com>
2019-12-17 14:01:55 +0100

    JAMES-3015 run integration tests on spring docker image

--- mpt/impl/imap-mailbox/external-james/src/test/java/org/apache/james/mpt/imapmailbox/external/james/DockerDeploymentValidationSpringJPAIT.java
+++ mpt/impl/imap-mailbox/external-james/src/test/java/org/apache/james/mpt/imapmailbox/external/james/DockerDeploymentValidationSpringJPAIT.java
@@ -19,32 +19,37 @@
 
 package org.apache.james.mpt.imapmailbox.external.james;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+
 import org.apache.james.core.Username;
 import org.apache.james.mpt.api.ImapHostSystem;
 import org.apache.james.mpt.imapmailbox.external.james.host.ProvisioningAPI;
 import org.apache.james.mpt.imapmailbox.external.james.host.SmtpHostSystem;
 import org.apache.james.mpt.imapmailbox.external.james.host.external.ExternalJamesConfiguration;
 import org.junit.After;
+import org.junit.Assume;
 import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-@Ignore("Not to be run on CI, as it will not use the current build")
-public class DockerDeploymentValidationSpringJPATest extends DeploymentValidation {
+public class DockerDeploymentValidationSpringJPAIT extends DeploymentValidation {
 
     private ImapHostSystem system;
     private SmtpHostSystem smtpHostSystem;
 
-    @Rule
-    public DockerJamesRule dockerJamesRule = new DockerJamesRule("linagora/james-jpa-spring");
+    private static String retrieveDockerImageName() {
+        String imageName = System.getProperty("docker.image.spring.jpa");
+        Assume.assumeThat("No property docker.image.spring.jpa defined to run integration-test", imageName, notNullValue());
+        return imageName;
+    }
+
+    private DockerJamesRule dockerJamesRule;
 
     @Override
     @Before
     public void setUp() throws Exception {
+        dockerJamesRule = new DockerJamesRule(retrieveDockerImageName());
 
         dockerJamesRule.start();
 
@@ -57,18 +62,6 @@ public class DockerDeploymentValidationSpringJPATest extends DeploymentValidatio
         system.beforeTest();
 
         super.setUp();
-    }
-
-    @Test
-    @Ignore("Not to be run on CI, as it will not use the current build. Uncomment to test on local dev environment")
-    @Override
-    public void validateDeployment() throws Exception {
-    }
-
-    @Test
-    @Ignore("Not to be run on CI, as it will not use the current build. Uncomment to test on local dev environment")
-    @Override
-    public void validateDeploymentWithMailsFromSmtp() throws Exception {
     }
 
     @Override
