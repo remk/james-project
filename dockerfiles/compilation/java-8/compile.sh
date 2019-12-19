cc2170077b4263b7e0491e99fdb7b46082388336
Matthieu Baechler <matthieu@apache.org>
2019-12-19 14:13:25 +0100

    fixup! JAMES-3015 run integration tests during build on jenkins

--- dockerfiles/compilation/java-8/compile.sh
+++ dockerfiles/compilation/java-8/compile.sh
@@ -60,7 +60,7 @@ git checkout $SHA1
 # Compilation
 
 if [ "$SKIPTESTS" = "skipTests" ]; then
-   mvn package -DskipTests ${MVN_ADDITIONAL_ARG_LINE}
+   mvn package -Djib.skip -Ddockerfile.skip -DskipTests ${MVN_ADDITIONAL_ARG_LINE}
 else
    mvn package verify ${MVN_ADDITIONAL_ARG_LINE}
 fi
