49f50c62520a7c6a870eda775b360372d2a8d864
Rémi KOWALSKI <rkowalski@linagora.com>
2020-01-16 17:30:04 +0100

    JAMES-3028 rename IOObjectStoreException to ObjectStoreIOException

--- server/blob/blob-memory/src/main/java/org/apache/james/blob/memory/MemoryDumbBlobStore.java
+++ server/blob/blob-memory/src/main/java/org/apache/james/blob/memory/MemoryDumbBlobStore.java
@@ -27,7 +27,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.james.blob.api.BlobId;
 import org.apache.james.blob.api.BucketName;
 import org.apache.james.blob.api.DumbBlobStore;
-import org.apache.james.blob.api.IOObjectStoreException;
+import org.apache.james.blob.api.ObjectStoreIOException;
 import org.apache.james.blob.api.ObjectNotFoundException;
 
 import com.google.common.base.Preconditions;
@@ -45,7 +45,7 @@ public class MemoryDumbBlobStore implements DumbBlobStore {
     }
 
     @Override
-    public InputStream read(BucketName bucketName, BlobId blobId) throws IOObjectStoreException, ObjectNotFoundException {
+    public InputStream read(BucketName bucketName, BlobId blobId) throws ObjectStoreIOException, ObjectNotFoundException {
         return readBytes(bucketName, blobId)
             .map(ByteArrayInputStream::new)
             .block();
@@ -72,7 +72,7 @@ public class MemoryDumbBlobStore implements DumbBlobStore {
                 try {
                     return IOUtils.toByteArray(inputStream);
                 } catch (IOException e) {
-                    throw new IOObjectStoreException("IOException occured", e);
+                    throw new ObjectStoreIOException("IOException occured", e);
                 }
             })
             .flatMap(bytes -> save(bucketName, blobId, bytes));
@@ -84,7 +84,7 @@ public class MemoryDumbBlobStore implements DumbBlobStore {
                 try {
                     return content.read();
                 } catch (IOException e) {
-                    throw new IOObjectStoreException("IOException occured", e);
+                    throw new ObjectStoreIOException("IOException occured", e);
                 }
             })
             .flatMap(bytes -> save(bucketName, blobId, bytes));
