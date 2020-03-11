6c32aee0adee456fae9b734392cc0530b683f64b
Rémi KOWALSKI <rkowalski@linagora.com>
2020-03-11 15:11:34 +0100

    fixup! JAMES-3082 use a specific RetryBackOffConfiguration for tests with a shorter first backoff delay

--- mailbox/api/src/main/java/org/apache/james/mailbox/events/RetryBackoffConfiguration.java
+++ mailbox/api/src/main/java/org/apache/james/mailbox/events/RetryBackoffConfiguration.java
@@ -22,7 +22,6 @@ package org.apache.james.mailbox.events;
 import java.time.Duration;
 import java.util.Objects;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 
