bc2cdfd5f310e8e889cfae99c7424833151103fc
Rémi Kowalski <rkowalski@linagora.com>
2020-07-31 10:17:03 +0200

    fixup! wip jmap set method

--- server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/mail/MailboxSet.scala
+++ server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/mail/MailboxSet.scala
@@ -1,3 +1,22 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *  http://www.apache.org/licenses/LICENSE-2.0                  *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
 package org.apache.james.jmap.mail
 
 import eu.timepit.refined.api.Refined
@@ -15,33 +34,26 @@ case class MailboxSetRequest(accountId: AccountId,
                              create: Option[Map[MailboxCreationId, JsObject]],
                              update: Option[Map[MailboxId, MailboxPatchObject]],
                              destroy: Option[Seq[MailboxId]],
-                             onDestroyRemoveEmails: Option[RemoveEmailsOnDestroy]
-                            )
-
+                             onDestroyRemoveEmails: Option[RemoveEmailsOnDestroy])
 
 object MailboxSetRequest {
   type MailboxCreationId = String Refined NonEmpty
 }
 
 case class RemoveEmailsOnDestroy(value: Boolean) extends AnyVal
-//TODO the parentId could be a backreference to a MailboxCreationId
-//TODO should we handle server-set parameters which should be allowed in request and that we should match to be the same than the response?
 case class MailboxCreationRequest(name: MailboxName, parentId: Option[MailboxId])
 
-
-//TODO strongly type when implementing update
 case class MailboxPatchObject(value: Map[String, JsObject])
 
 case class MailboxSetResponse(accountId: AccountId,
                               oldState: Option[State],
                               newState: State,
                               created: Option[Map[MailboxCreationId, MailboxCreationResponse]],
-                              updated: Option[Map[MailboxId, MailboxUpdateResponse]],//TODO could be a backreference to a MailboxCreationId
-                              destroyed: Option[Seq[MailboxId]],//TODO could be a backreference to a MailboxCreationId,
+                              updated: Option[Map[MailboxId, MailboxUpdateResponse]],
+                              destroyed: Option[Seq[MailboxId]],
                               notCreated: Option[Map[MailboxCreationId, MailboxSetError]],
                               notUpdated: Option[Map[MailboxId, MailboxSetError]],
-                              notDestroyed: Option[Map[MailboxId, MailboxSetError]]
-                             )
+                              notDestroyed: Option[Map[MailboxId, MailboxSetError]])
 
 case class MailboxSetError(`type`: SetErrorType, description: Option[SetErrorDescription], properties: Option[Properties])
 
@@ -58,5 +70,4 @@ case class MailboxCreationResponse(id: MailboxId,
                                    isSubscribed: IsSubscribed
                                   )
 
-//TODO strongly type when implementing update
 case class MailboxUpdateResponse(value: JsObject)
