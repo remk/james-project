7ba942b473e4d7218977ba5c87827078b329d6b6
Rémi Kowalski <rkowalski@linagora.com>
2020-09-23 10:29:36 +0200

    JAMES-3386 do not allow blank mailbox path

--- mailbox/api/src/main/java/org/apache/james/mailbox/model/MailboxPath.java
+++ mailbox/api/src/main/java/org/apache/james/mailbox/model/MailboxPath.java
@@ -25,6 +25,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.james.core.Username;
 import org.apache.james.mailbox.DefaultMailboxes;
 import org.apache.james.mailbox.MailboxSession;
@@ -194,10 +195,11 @@ public class MailboxPath {
 
     boolean hasEmptyNameInHierarchy(char pathDelimiter) {
         String delimiterString = String.valueOf(pathDelimiter);
-        return this.name.isEmpty()
-            || this.name.contains(delimiterString + delimiterString)
-            || this.name.startsWith(delimiterString)
-            || this.name.endsWith(delimiterString);
+        String nameWithoutSpaces = name.replaceAll("\\s", "");
+        return StringUtils.isBlank(nameWithoutSpaces)
+            || nameWithoutSpaces.contains(delimiterString + delimiterString)
+            || nameWithoutSpaces.startsWith(delimiterString)
+            || nameWithoutSpaces.endsWith(delimiterString);
     }
 
     public String asString() {
